d0179783dc46053a3f56f8d484cd10b6b1931d47
BGMarinho <bgmarinho96@gmail.com>
2024-07-29 13:58:13 -0300

    Starting the Footer.

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles';
+
+export default function Footer() {
+  return <S.FooterWrapper>Teste do footer</S.FooterWrapper>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
-import NewFueling from './components/NewFueling';
 import TopBar from './components/TopBar';
+import NewFueling from './components/NewFueling';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <main>
       <TopBar />
       <NewFueling />
+      <Footer />
     </main>
   );
 }
