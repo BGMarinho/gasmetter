8c6dafd085a3a6798e5f76430bb29c9052625a21
BGMarinho <bgmarinho96@gmail.com>
2024-08-01 16:18:56 -0300

    feat(Footer): Fixing some style erros and implementing new stylization on Footer.

--- app/components/Footer/styles.ts
+++ app/components/Footer/styles.ts
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const FooterWrapper = styled.section`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  position: absolute;
-  bottom: 0;
   background-color: #000000;
   color: #ffffff;
+  border-top: 5px solid #191970;
   line-height: 1rem;
   font-size: 0.75rem;
-  width: 100%;
 `;
 
 export const SocialNetworks = styled.div`
@@ -22,7 +24,7 @@ export const SocialNetworks = styled.div`
 `;
 
 export const Information = styled.div`
-  font-size: 1rem;
+  font-size: 0.75rem;
   a {
     all: unset;
   }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ width: '100vw' }}>
       <TopBar />
       <NewFueling />
       <Footer />
