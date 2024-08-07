e66bcf7defd01b9ea859f395891926cfcf2f1b47
BGMarinho <bgmarinho96@gmail.com>
2024-08-07 16:28:38 -0300

    feat(Formulary): Setting the Formulary component with just the basic structure for further implementations.

---
+++ app/components/Formulary/Formulary.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles';
+interface FormularyProps {}
+export default function Formulary() {
+  return <S.FormularyWrapper>oioio</S.FormularyWrapper>;
+}
+
+{
+  /* <h1>Titulo</h1>
+<h3>frase para introduzir o preenchimento do form</h3>
+<form action="">
+  <label htmlFor="">posto</label>
+  <input type="text" />
+  <label htmlFor="">valor por litro</label>
+  <input type="number" />
+  <label htmlFor="">valor abastecido</label>
+  <input type="number" />
+  <label htmlFor="">tipo de combustível</label>
+  <input type="text" />
+  <button>salvar</button>
+</form> */
+}
