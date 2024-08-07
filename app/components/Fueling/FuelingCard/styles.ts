17f74e97c67b8dfecfbb66357dc1327dbdc01da3
BGMarinho <bgmarinho96@gmail.com>
2024-08-07 00:25:31 -0300

    feat(FuelingCard): Starting the stylization of the card.

--- app/components/Fueling/FuelingCard/styles.ts
+++ app/components/Fueling/FuelingCard/styles.ts
@@ -4,7 +4,7 @@ export const FuelingCard = styled.div`
   height: 70%;
   width: 70%;
   padding: 0.75rem;
-  border: 2px solid black;
-  border-radius: 4%;
-  box-shadow: none;
+  border: 1px solid black;
+  border-radius: 3% 3% 0% 3%;
+  box-shadow: -3px 3px 4px 2px #e0e0e0;
 `;
