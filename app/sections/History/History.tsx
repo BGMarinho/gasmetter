2a488eb7a9199770b1df27ae26763e9ac9bf0111
BGMarinho <bgmarinho96@gmail.com>
2024-08-09 14:56:46 -0300

    feat (History): Creating the basic structure of the History section.

---
+++ app/sections/History/History.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles';
+
+export default function History() {
+  return <S.HistorySection>oio</S.HistorySection>;
+}
---
+++ app/sections/History/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const HistorySection = styled.section``;
